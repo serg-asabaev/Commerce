ef2d404ac5b8dd30a4c3876c19374353cec82049
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-04-02 20:10:34 +0400

    added class Smarphone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.product import Product
+from src.product import Product, Smartphone
 from src.category import Category
 
 if __name__ == "__main__":
@@ -6,51 +6,14 @@ if __name__ == "__main__":
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-    sum_pr_1_2 = product1 + product2
-    print(sum_pr_1_2)
+    sm_1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, '2,25 Ггц',\
+                      'Galaxy S23 Ultra', '256GB', 'Серый')
 
-    # print(product1.name)
-    # print(product1.description)
-    print(product1.price)
-    product1.price = 170000.0
-    print(product1.price)
-    # print(product1.quantity)
-    #
-    # print(product2.name)
-    # print(product2.description)
-    # print(product2.price)
-    # print(product2.quantity)
-    #
-    # print(product3.name)
-    # print(product3.description)
-    # print(product3.price)
-    # print(product3.quantity)
-
-    product_dict = {'name': 'Xiaomi Redmi Note 11', 'description': '"1024GB, Синий"', 'price': 31000.0, 'quantity': 14}
-    product5 = Product.new_product(product_dict)
-
-    print(product5.name)
-    print(product5.description)
-    print(product5.price)
-    print(product5.quantity)
-
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
-
-    # print(category1.name == "Смартфоны")
-    # print(category1.description)
-    # print(len(category1.products))
-    # print(category1.category_count)
-    # print(category1.product_count)
-    print(category1)
-
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
-
-    # print(category2.name)
-    # print(category2.description)
-    # print(len(category2.products))
-    print(category2)
+    print(sm_1.name)
+    print(sm_1.description)
+    print(sm_1.price)
+    print(sm_1.quantity)
+    print(sm_1.efficiency)
+    print(sm_1.model)
+    print(sm_1.memory)
+    print(sm_1.color)
--- src/product.py
+++ src/product.py
@@ -33,3 +33,23 @@ class Product:
         """ Сложение сумм всех продуктов в категории"""
         res = (self.__price * self.quantity) + (other.price * other.quantity)
         return res
+
+
+class Smartphone(Product):
+    efficiency = ''
+    model = ''
+    memory = ''
+    color = ''
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    country = ''
+    germination_period = ''
+    color = ''
